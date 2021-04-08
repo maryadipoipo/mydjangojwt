56009471966442e6c43a098c2f69ed60655c192c
maryadikpi <maryadi@kpiinstitute.org>
2021-04-08 14:34:48 +0700

    success get token from login postman

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializer import UserRegistrationSerializer, UserLoginSerializer
